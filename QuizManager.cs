35aaeab309cd85859fd1fca72534429a1ef57457
nikponomarenko <Nikita.Ponomarenko@urfu.me>
2021-06-16 01:09:29 +0500

    Code Files
    Кодовые файлы проекта

---
+++ QuizManager.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizManager : MonoBehaviour
+{
+#pragma warning disable 649
+    [SerializeField] private QuizGameUI quizGameUI;
+    [SerializeField] private List<QuizDataScriptable> quizDataList;
+    [SerializeField] private float timeInSeconds;
+#pragma warning restore 649
+
+    private string currentCategory = "";
+    private int correctAnswerCount = 0;
+    private List<Question> questions;
+    private Question selectedQuetion = new Question();
+    private int lifesRemaining;
+    private float currentTime;
+    private QuizDataScriptable dataScriptable;
+
+    private GameStatus gameStatus = GameStatus.NEXT;
+
+    public GameStatus GameStatus { get { return gameStatus; } }
+
+    public List<QuizDataScriptable> QuizData { get => quizDataList; }
+
+    public void StartGame(int categoryIndex, string category)
+    {
+        currentCategory = category;
+        correctAnswerCount = 0;
+        lifesRemaining = 3;
+        currentTime = timeInSeconds;
+        questions = new List<Question>();
+        dataScriptable = quizDataList[categoryIndex];
+        questions.AddRange(dataScriptable.questions);
+        SelectQuestion();
+        gameStatus = GameStatus.PLAYING;
+    }
+
+    /// <summary>
+    /// Method used to randomly select the question form questions data
+    /// </summary>
+    private void SelectQuestion()
+    {
+        int val = UnityEngine.Random.Range(0, questions.Count);
+        selectedQuetion = questions[val];
+        quizGameUI.SetQuestion(selectedQuetion);
+
+        questions.RemoveAt(val);
+    }
+
+    private void Update()
+    {
+        if (gameStatus == GameStatus.PLAYING)
+        {
+            currentTime -= Time.deltaTime;
+            SetTime(currentTime);
+        }
+    }
+
+    void SetTime(float value)
+    {
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        quizGameUI.TimerText.text = time.ToString("mm':'ss");
+
+        if (currentTime <= 0)
+        {
+            GameEnd();
+        }
+    }
+
+    /// <summary>
+    /// Method called to check the answer is correct or not
+    /// </summary>
+    /// <param name="selectedOption">answer string</param>
+    /// <returns></returns>
+    public bool Answer(string selectedOption)
+    {
+        bool correct = false;
+        if (selectedQuetion.correctAns == selectedOption)
+        {
+            correctAnswerCount++;
+            correct = true;
+        }
+        else
+        {
+            lifesRemaining--;
+            quizGameUI.ReduceLife(lifesRemaining);
+
+            if (lifesRemaining == 0)
+            {
+                GameEnd();
+            }
+        }
+
+        if (gameStatus == GameStatus.PLAYING)
+        {
+            if (questions.Count > 0)
+            {
+                Invoke("SelectQuestion", 0.4f);
+            }
+            else
+            {
+                GameEnd();
+            }
+        }
+        return correct;
+    }
+
+    private void GameEnd()
+    {
+        gameStatus = GameStatus.NEXT;
+        quizGameUI.GameOverPanel.SetActive(true);
+        PlayerPrefs.SetInt(currentCategory, correctAnswerCount); //save the score for this category
+    }
+}
+
+[System.Serializable]
+public class Question
+{
+    public string questionInfo;
+    public QuestionType questionType;
+    public Sprite questionImage;
+    public AudioClip audioClip;
+    public UnityEngine.Video.VideoClip videoClip;
+    public List<string> options;
+    public string correctAns;
+}
+
+[System.Serializable]
+public enum QuestionType
+{
+    TEXT,
+    IMAGE,
+    AUDIO,
+    VIDEO
+}
+
+[SerializeField]
+public enum GameStatus
+{
+    PLAYING,
+    NEXT
+}
